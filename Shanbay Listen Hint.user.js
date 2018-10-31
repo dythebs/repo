daf9d0e83a71c6113fbedac5a11777687e474547
dythebs <244271172@qq.com>
2018-10-31 17:24:41 +0800

    change match url

--- Shanbay Listen Hint.user.js
+++ Shanbay Listen Hint.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Shanbay Listen Hint
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Yang
-// @match        https://www.shanbay.com/listen/sentence/
+// @match        https://www.shanbay.com/listen/*
 // @grant        none
 // ==/UserScript==
 
